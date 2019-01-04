9c7e63d86addcf14f459ab8147d1105a4bd2128b
lilkho <mkhondker@stuy.edu>
2019-01-04 14:31:58 -0500

    created methods

---
+++ Rule.java
@@ -0,0 +1,23 @@
+public class Rule {
+  String name;
+  String description;
+
+  public Rule(String name, String description) {
+    this.name = name;
+    this.name = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+
+  }
+
+  public String toString() {
+    return name+": "+description;
+  }
+
+}
