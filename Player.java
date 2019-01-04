3101f65463fb7d19d0a2669f1b133fbbb85e4d6f
lilkho <mkhondker@stuy.edu>
2019-01-04 14:27:01 -0500

    empty methods added

---
+++ Player.java
@@ -0,0 +1,25 @@
+public class Player {
+  String name;
+  ArrayList<Card> cards;
+  int numCards;
+
+  public Player(String name, int numCards) {
+
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getNumCards() {
+
+  }
+
+  public ArrayList<Card> getCards() {
+
+  }
+
+  public String toString() {
+
+  }
+}
