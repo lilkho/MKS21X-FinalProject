f2d40294abeb50af5e5da58e1c84ea555a5d8b21
lilkho <mkhondker@stuy.edu>
2019-01-04 14:50:15 -0500

    removing terminal stuff

--- Card.java
+++ Card.java
@@ -1,17 +1,3 @@
-import com.googlecode.lanterna.terminal.Terminal.SGR;
-import com.googlecode.lanterna.TerminalFacade;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.Key.Kind;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.Terminal.Color;
-import com.googlecode.lanterna.terminal.TerminalSize;
-import com.googlecode.lanterna.LanternaException;
-import com.googlecode.lanterna.input.CharacterPattern;
-import com.googlecode.lanterna.input.InputDecoder;
-import com.googlecode.lanterna.input.InputProvider;
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.KeyMappingProfile;
-
 public class Card{
   private String color;
   private String value;
@@ -39,24 +25,6 @@ public class Card{
   }
 
   public String toString(){
-    Terminal terminal = TerminalFacade.createTextTerminal();
-		terminal.enterPrivateMode();
-
-    boolean running = true;
-
-    while(running){
-      Key key = terminal.readInput();
-      if (key != null)
-      {
-
-        if (key.getKind() == Key.Kind.Escape) {
-
-          terminal.exitPrivateMode();
-          running = false;
-        }
-      }
-      terminal.applyBackgroundColor(Terminal.Color.WHITE);
-      terminal.applyForegroundColor(Terminal.Color.BLACK);
-    }
+    return "";
   }
 }
