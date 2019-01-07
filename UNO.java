6007ea2adc6ab51a25e2900a648dadf4dc6cbbd1
tshi3 <tshi3@stuy.edu>
2019-01-07 00:08:58 -0500

    changed draw and play methods to require player, added deck field

--- UNO.java
+++ UNO.java
@@ -1,24 +1,33 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class UNO{
   private boolean order;
   private Player turn;
-  private ArrayList<String> players;
+  private ArrayList<Player> players;
   private Card card;
+  private ArrayList<Card> deck = new ArrayList<Card>();
+  private Random randgen;
 
   public UNO(int numPlayers, int numRules){
-    players = new ArrayList<String>[numPlayers];
-
+    players = new ArrayList<Player>(numPlayers);
+    
   }
 
-  public void draw(int num){
-    this.cards.add();
-  }
-
-  public void play(Card toPlay){
-    card = toPlay;
-    if(toPlay.playable()){
-      this.cards.remove(toPlay);
+  public void draw(Player person, int num){
+    for(int x=0; x<num; x++){
+      Card toAdd = deck.get(Math.abs(randgen.nextInt(deck.size())));
+      person.addCard(toAdd);
     }
+  }
+
+  public void play(Player person, Card toPlay){
+    if(toPlay.playable(toPlay)){
+      person.removeCard(toPlay);
+    }
+  }
+  
+  public void setTurn(Player person){
+    turn = person;
   }
 }
