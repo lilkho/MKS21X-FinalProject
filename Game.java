6eff979212c1bef778aee20e1ed070a5b8e1f3cd
Tina Shi <tshi3@stuy.edu>
2019-01-07 14:46:17 -0500

    added refresh method

--- Game.java
+++ Game.java
@@ -12,10 +12,15 @@ public class Game{
 
   public Game(int numPlayers, int numRules){
     players = new ArrayList<Player>(numPlayers);
+    for(int x=0; x<numPlayers; x++){
 
+    }
   }
 
   public void draw(Player person, int num){
+    if(deck.size()==0){
+      refresh();
+    }
     for(int x=0; x<num; x++){
       //chooses a random card from the deck and adds it to the player's hand
       Card toAdd = deck.get(Math.abs(randgen.nextInt(deck.size())));
@@ -24,6 +29,13 @@ public class Game{
       discard.add(toAdd);
       deck.remove(toAdd);
     }
+  }
+
+  public void refresh(){
+    for(int x=0; x<discard.size(); x++){
+      deck.set(x, discard.get(x));
+    }
+    discard.clear();
   }
 
   public void play(Player person, Card toPlay){
