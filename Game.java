de3dae35d6ea1ca2b3d7e1ab60a957a0112fb262
Tina Shi <tshi3@stuy.edu>
2019-01-07 14:30:48 -0500

    changed UNO to Game, made new UNO class

---
+++ Game.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Game{
+  private boolean order;
+  private Player turn;
+  private ArrayList<Player> players;
+  private Card card;
+  private ArrayList<Card> deck = new ArrayList<Card>();
+  private ArrayList<Card> discard = new ArrayList<Card>();
+  private Random randgen;
+
+  public Game(int numPlayers, int numRules){
+    players = new ArrayList<Player>(numPlayers);
+
+  }
+
+  public void draw(Player person, int num){
+    for(int x=0; x<num; x++){
+      //chooses a random card from the deck and adds it to the player's hand
+      Card toAdd = deck.get(Math.abs(randgen.nextInt(deck.size())));
+      person.addCard(toAdd);
+      //removes card from deck and adds it to "discard" pile
+      discard.add(toAdd);
+      deck.remove(toAdd);
+    }
+  }
+
+  public void play(Player person, Card toPlay){
+    if(toPlay.playable(toPlay)){
+      person.removeCard(toPlay);
+    }
+  }
+
+  public void setTurn(Player person){
+    turn = person;
+  }
+}
--- UNO.java
+++ UNO.java
@@ -1,38 +1,5 @@
-import java.util.ArrayList;
-import java.util.Random;
-
 public class UNO{
-  private boolean order;
-  private Player turn;
-  private ArrayList<Player> players;
-  private Card card;
-  private ArrayList<Card> deck = new ArrayList<Card>();
-  private ArrayList<Card> discard = new ArrayList<Card>();
-  private Random randgen;
-
-  public UNO(int numPlayers, int numRules){
-    players = new ArrayList<Player>(numPlayers);
+  public static void main(String[] args){
     
-  }
-
-  public void draw(Player person, int num){
-    for(int x=0; x<num; x++){
-      //chooses a random card from the deck and adds it to the player's hand
-      Card toAdd = deck.get(Math.abs(randgen.nextInt(deck.size())));
-      person.addCard(toAdd);
-      //removes card from deck and adds it to "discard" pile
-      discard.add(toAdd);
-      deck.remove(toAdd);
-    }
-  }
-
-  public void play(Player person, Card toPlay){
-    if(toPlay.playable(toPlay)){
-      person.removeCard(toPlay);
-    }
-  }
-  
-  public void setTurn(Player person){
-    turn = person;
   }
 }
