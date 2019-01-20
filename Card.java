996dc1a05373882ae971641948434ea52db9f9e4
Tina Shi <tshi3@stuy.edu>
2019-01-20 14:33:47 -0500

    javadoc for card

--- Card.java
+++ Card.java
@@ -2,23 +2,30 @@ public class Card{
   private String color;
   private String value;
 
+  /**
+  * Constructor for card
+  * @param col color of card
+  * @param val value of card
+  */
   public Card(String col, String val){
     color = col;
     value = val;
   }
 
-  public String getColor(){
-    return color;
-  }
-
-  public String getValue(){
-    return value;
-  }
-
+  /**
+  * Sets color of card to given color
+  * @param col color to be set to
+  */
   public void setColor(String col){
     color = col;
   }
 
+  /**
+  * Returns the type of a card
+  * @param check card to check type of
+  * @return "NUMBER" if the card is a number,
+            "ACTION" otherwise
+  */
   public String getType(Card check){
     String s = check.getValue();
     if(s.equals("0") || s.equals("1") || s.equals("2") || s.equals("3") || s.equals("4") || s.equals("5") ||
@@ -29,20 +36,39 @@ public class Card{
     }
   }
 
+  /**
+  * Returns if the card is playable
+  * @param compare card to be compared against the top card
+  * @return true if the card is playable,
+  *         false otherwise
+  */
   public boolean playable(Card compare){
     //to prevent other cards to be placed on +2 or +4 bc they share the same color
     /*if(this.getValue().equals("+2") && !compare.getValue().equals("+2") ||
       this.getValue().equals("+4") && !compare.getValue().equals("+4")){
         return false;
     }*/
-      //black can be placed on any card, same color or value is playable
+    //black can be placed on any card, same color or value is playable
     if (this.getColor().equals("BLACK")) {return true;}
     return compare.getColor().equals("BLACK") ||
       this.getColor().equals(compare.getColor()) ||
       this.getValue().equals(compare.getValue());
   }
 
+  /**
+  * Returns card info
+  * @return card's color and value
+  */
   public String toString(){
     return ""+color+value;
+  }
+
+  //GET METHODS//
+  public String getColor(){
+    return color;
+  }
+
+  public String getValue(){
+    return value;
   }
 }
