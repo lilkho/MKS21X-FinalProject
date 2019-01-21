6fd7902171b480740161a715f01ec729de291554
Tina Shi <tshi3@stuy.edu>
2019-01-21 00:35:21 -0500

    added getType method to return action/inaction based on the type of rule

--- Rule.java
+++ Rule.java
@@ -13,11 +13,32 @@ public class Rule {
   }
 
   /**
-  * Returns rule info (name, description)
+  * Prints rule info (name, description)
   * @return rule's name and description
   */
   public String toString() {
     return name+": "+description;
+  }
+
+  /**
+  * Prints rule type (action, inaction)
+  * @param r rule to check
+  * @return rule type
+  */
+  public String getType(Rule r) {
+    if(r.getName().equals("BOMB CARD") ||
+      r.getName().equals("SUDDEN DEATH CARD") ||
+      r.getName().equals("INK CARD") ||
+      r.getName().equals("EQUALITY CARD") ||
+      r.getName().equals("RAIN CARD") ||
+      r.getName().equals("CLONE CARD") ||
+      r.getName().equals("JUSTICE CARD") ||
+      r.getName().equals("THUNDER CARD") ||
+      r.getName().equals("MAGNET CARD")){
+      return "ACTION";
+    }else{
+      return "INACTION";
+    }
   }
 
   //GET METHODS//
